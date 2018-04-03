64370125cce1f2eff7e261a5cea41a097cf3ece1
Yohan Legat <yohan.legat@blablacar.com>
2018-04-03 16:18:08 +0200

    Fix InMemoryEventBus
    Events where not applied with the right order

--- infrastructure/src/main/java/com/github/ylegat/workshop/infrastructure/InMemoryEventBus.java
+++ infrastructure/src/main/java/com/github/ylegat/workshop/infrastructure/InMemoryEventBus.java
@@ -1,15 +1,26 @@
 package com.github.ylegat.workshop.infrastructure;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import com.github.ylegat.workshop.domain.common.Event;
 import com.github.ylegat.workshop.domain.common.EventBus;
 import com.github.ylegat.workshop.domain.common.EventListener;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 public class InMemoryEventBus implements EventBus {
 
     private final Set<EventListener> listeners = new HashSet<>();
+
+    private final ScheduledExecutorService executorService;
+
+    public InMemoryEventBus() {
+        executorService = newSingleThreadScheduledExecutor(new ThreadFactoryBuilder().setNameFormat("event-bus-%s").build());
+    }
 
     @Override
     public void register(EventListener eventListener) {
@@ -18,7 +29,7 @@ public class InMemoryEventBus implements EventBus {
 
     @Override
     public void push(List<Event> events) {
-        events.forEach(event -> listeners.forEach(event::applyOn));
+        executorService.submit(() -> events.forEach(event -> listeners.forEach(event::applyOn)));
     }
 
     @Override
